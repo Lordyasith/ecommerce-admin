1d41cc116653cc323fb8f47a5efafbca1dc594ea
sanjayguwaju <sanjayguwaju@gmail.com>
2023-11-19 17:38:05 +0545

    chore: modal components completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state)=> state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(()=> {
+    if(!isOpen) {
+      onOpen();
+    }
+  },[isOpen,onOpen]);
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/"/>
+      Root Page
     </div>
   )
 }
