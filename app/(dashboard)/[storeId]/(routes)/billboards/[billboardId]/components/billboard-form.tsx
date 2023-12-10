bd9dcf4adcecc95fde4f49c3f65f31148d02712b
sanjayguwaju <sanjayguwaju@gmail.com>
2023-12-10 15:11:24 +0545

    chore: data-table-admin

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -23,7 +23,6 @@ import {
 } from "@/components/ui/form";
 import  { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
 import ImageUpload from "@/components/ui/image-upload";
 
@@ -43,7 +42,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
 }) => {
     const params = useParams();
     const router = useRouter();
-    const origin = useOrigin();
 
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
@@ -69,9 +67,9 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
             } else {
                 await axios.post(`/api/${params.storeId}/billboards`, data);
             }
-            router.refresh();
             router.push(`/${params.storeId}/billboards`)
             toast.success(toastMessage);
+            router.refresh();
         } catch (error) {
             toast.error("Something went wrong.");
         } finally {
@@ -83,9 +81,9 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
         try {
             setLoading(true);
             await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
-            router.refresh();
-            router.push("/");
+            router.push(`/${params.storeId}/billboards`);
             toast.success("Billboard deleted.");
+            router.refresh();
         } catch (error) {
             toast.error("Make sure you removed all categories using this billboard first.");
         } finally {
@@ -161,7 +159,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
                 </Button>
             </form>
         </Form>
-        <Separator/>
        </>
     )
 }
